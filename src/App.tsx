dc2cd08d3dc02d9dd38102aadaab9566bcb6de14
Андрій <gapeev1979@gmail.com>
2026-01-14 22:27:05 +0200

    add container

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import './App.css';
+import css from './App.module.css';
 // import Button from './components/Button/Button';
 import ModalOn from './components/Modal/ModalOn';
 
 function App() {
   return (
     <>
-      <h1>START PETLOVE SITE</h1>
+      <section className={css.container}>
+        <h1>START PETLOVE SITE</h1>
 
-      <ModalOn />
-      {/* <Button variant="primary" text="Login" /> */}
-      {/* <Button variant="secondary" text="Follow" /> */}
+        <ModalOn />
+        {/* <Button variant="primary" text="Login" /> */}
+        {/* <Button variant="secondary" text="Follow" /> */}
+      </section>
     </>
   );
 }
